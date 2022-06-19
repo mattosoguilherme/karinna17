f2ccd0728b33c3e967a81c8dec0746e9bc04f437
Guilherme Mattoso <guilhermemktfran@gmail.com>
2022-06-19 19:10:44 -0300

    ..

--- index.js
+++ index.js
@@ -2,14 +2,11 @@
 const express = require("express");
 const { url } = require("inspector");
 // Importação do módulo nodemailer
-const nodemailer = require("nodemailer");
-const smtpTransport = require("nodemailer/lib/smtp-transport");
 
-require("dotenv").config();
 
 // Instancia uma referência do express no projeto
 const app = express();
-const port = process.env.PORT || 3000; // Const para armanezar a porta do servidor
+const port = process.env.PORT || 3000;// Const para armanezar a porta do servidor
 app.set("view engine", "ejs");
 const path = require("path");
 app.use(express.urlencoded({ extended: true }))
