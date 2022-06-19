2366119d9d702e7a5035b695c30c138eb76f9398
Guilherme Mattoso <guilhermemktfran@gmail.com>
2022-06-19 18:00:11 -0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
-* Template Name: ComingSoon - v4.6.0
-* Template URL: https://bootstrapmade.com/comingsoon-free-html-bootstrap-template/
+* Template Name: WeBuild - v4.6.1
+* Template URL: https://bootstrapmade.com/free-bootstrap-coming-soon-template-countdwon/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
@@ -20,57 +20,23 @@
   }
 
   /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
-
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
-
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
-
-  /**
    * Countdown timer
    */
+
   let countdown = select('.countdown');
-  const output = countdown.innerHTML;
 
   const countDownDate = function() {
     let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
 
+    let weeks = Math.floor(timeleft / (1000 * 60 * 60 * 24 * 7));
     let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
     let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-    countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
+    let output = countdown.getAttribute('data-template');
+    output = output.replace('%w', weeks).replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
+    countdown.innerHTML = output;
   }
   countDownDate();
   setInterval(countDownDate, 1000);
